02cfca41333ef7a44fe6d81110e2b96efc4183fd
jojinwon96 <jojinwon960328@gmail.com>
2023-04-24 15:16:59 +0900

    자바 커리큘럼 폴더생성

--- YIST/src/main/java/com/kh/yist/instructor/controller/instructorController.java
+++ YIST/src/main/java/com/kh/yist/instructor/controller/instructorController.java
@@ -99,6 +99,24 @@ public class instructorController {
 		return "";
 	}
 	
+	public void createFolder() {
+		String path = "D:\\Eclipse\\Java\\새폴더"; //폴더 경로
+		File Folder = new File(path);
+
+		// 해당 디렉토리가 없을경우 디렉토리를 생성합니다.
+		if (!Folder.exists()) {
+			try{
+			    Folder.mkdir(); //폴더 생성합니다.
+			    System.out.println("폴더가 생성되었습니다.");
+		        } 
+		        catch(Exception e){
+			    e.getStackTrace();
+			}        
+	         }else {
+			System.out.println("이미 폴더가 생성되어 있습니다.");
+		}
+	}
+	
 	public String saveFile(MultipartFile upfile, HttpSession session) {
 
 		String originName = upfile.getOriginalFilename(); // flower.png
