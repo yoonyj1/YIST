76dccafbafd9d0be715de9fb729d864ef44605a7
kimpompu <kafka206@gmail.com>
2023-05-17 18:35:05 +0900

    commit
    commit

--- YIST/src/main/java/com/kh/yist/admin/AdminCotroller.java
+++ YIST/src/main/java/com/kh/yist/admin/AdminCotroller.java
@@ -1,5 +1,7 @@
 package com.kh.yist.admin;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -9,11 +11,18 @@ import org.springframework.web.servlet.ModelAndView;
 public class AdminCotroller {
 
 	
-
+	@Autowired
+	private BCryptPasswordEncoder bcryptPasswordEncoder;
 	
 
-	
-	
+	@RequestMapping("encode.me")
+	public String getEncodePwd(String pwd) {
+		
+		String encPwd = bcryptPasswordEncoder.encode(pwd);
+		System.out.println(encPwd);
+		
+		return "redirect:/";
+	}
 
 	
 }
